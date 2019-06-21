32a9b2a7ff4165f2e8953425f05fb3d95ffb59c5
wk <wombatukun@gmail.com>
2019-06-21 09:18:52 +0700

    multiproject build, urlencode searchstring

--- src/main/java/wombatukun/tests/test10/services/SearcherService.java
+++
@@ -1,9 +0,0 @@
-package wombatukun.tests.test10.services;
-
-import wombatukun.tests.test10.dto.ResponseDto;
-
-public interface SearcherService {
-
-	ResponseDto searchQuestionsByTitle(String value, Integer page, Integer pageSize);
-
-}
