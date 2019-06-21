5d19093ed0d4eca98691f80ab8a6c0171a03b39b
wk <wombatukun@gmail.com>
2019-06-21 10:57:38 +0700

    added actuators and hystrix dashboard

--- searcher-web/src/main/java/wombatukun/tests/test10/Application.java
+++ searcher-web/src/main/java/wombatukun/tests/test10/Application.java
@@ -3,9 +3,11 @@ package wombatukun.tests.test10;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableCaching
+@EnableHystrixDashboard
 public class Application {
 
 	public static void main(String[] args) {
