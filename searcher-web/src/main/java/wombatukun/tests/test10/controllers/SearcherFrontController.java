c47d68772a32eac6b26b6675753ebfbaa50a774c
wk <wombatukun@gmail.com>
2019-06-23 17:09:05 +0700

    fixed thymeleaf pagination

--- searcher-web/src/main/java/wombatukun/tests/test10/controllers/SearcherFrontController.java
+++ searcher-web/src/main/java/wombatukun/tests/test10/controllers/SearcherFrontController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import wombatukun.tests.test10.dto.RequestDto;
 import wombatukun.tests.test10.dto.ResponseDto;
+import wombatukun.tests.test10.forms.SearchForm;
 import wombatukun.tests.test10.services.SearcherService;
 
 import javax.validation.Valid;
@@ -36,24 +36,20 @@ public class SearcherFrontController {
 
 	@GetMapping(value = "thymeleaf")
 	public String thymeleafFront(Model model) {
-		model.addAttribute("searchForm", new RequestDto(null, 1, 30));
+		model.addAttribute("searchForm", new SearchForm(null, null, 1, 30));
 		return TMPL_THYMELEAF;
 	}
 
 	@PostMapping(value = "thymeleaf")
 	public String thymeleafSearch(
-			@Valid @ModelAttribute("searchForm") RequestDto request,
+			@Valid @ModelAttribute("searchForm") SearchForm form,
 			@RequestParam(value="action") String action,
 			BindingResult result,
 			Model model
 	) {
 		if (!result.hasErrors()) {
-			switch(action) {
-				case "prevPage": request.setPage(request.getPage()-1); break;
-				case "nextPage": request.setPage(request.getPage()+1); break;
-				default: request.setPage(1);
-			}
-			ResponseDto response = searcherService.searchQuestionsByTitle(request.getValue(), request.getPage(), request.getPageSize());
+			turnThePage(action, form);
+			ResponseDto response = searcherService.searchQuestionsByTitle(form.getValue(), form.getPage(), form.getPageSize());
 			model.addAttribute("errorMessage", response.getErrorMessage());
 			model.addAttribute("items", response.getItems());
 			model.addAttribute("hasMore", response.isHasMore());
@@ -61,9 +57,22 @@ public class SearcherFrontController {
 			model.addAttribute("pageSize", response.getPageSize());
 			model.addAttribute("total", response.getTotal());
 			model.addAttribute("searchString", response.getSearchString());
-			model.addAttribute("searchForm", new RequestDto(request.getValue(), request.getPage(), request.getPageSize()));
+			model.addAttribute("searchForm", new SearchForm(form.getValue(), form.getValue(), form.getPage(), form.getPageSize()));
 		}
 		return TMPL_THYMELEAF;
+	}
+
+	private void turnThePage(String action, SearchForm form) {
+		if ("search".equals(action)) {
+			form.setPage(1);
+		} else {
+			form.setValue(form.getPrevValue());
+			switch(action) {
+				case "prevPage": form.setPage(form.getPage()-1); break;
+				case "nextPage": form.setPage(form.getPage()+1); break;
+				default: log.warn("Unknown action: {}", action);
+			}
+		}
 	}
 
 }
