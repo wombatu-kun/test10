e4f58b1f3ddae559c96a85afe03c49caf5500cd0
wk <wombatukun@gmail.com>
2019-06-26 15:59:09 +0700

    added swagger api documentation and parallel test execution

--- searcher-web/src/main/java/wombatukun/tests/test10/Application.java
+++ searcher-web/src/main/java/wombatukun/tests/test10/Application.java
@@ -4,11 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
 @EnableHystrixDashboard
+@EnableSwagger2
 public class Application {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+				.paths(PathSelectors.any())
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
